dbf7a330c76ae3fe704eff84aef447afd0ec0a60
ocmwdt <ocmwdt@gmail.com>
2018-05-15 23:01:27 +0400

    three conv layers have been added

--- mnist_sm.py
+++ mnist_sm.py
@@ -8,7 +8,7 @@ from tensorflow.python.tools import inspect_checkpoint as chkp
 print("Tensorflow version " + tf.__version__)
 
 
-def conv_layer(input, filterX, filterY, inChannels, outChannels, name='convolutional'):
+def conv_layer(input, filterX, filterY, inChannels, outChannels, xStride=1, yStride=1, name='convolutional'):
     with tf.name_scope(name) as scope:
         w = tf.Variable(tf.truncated_normal([filterX, filterY, inChannels, outChannels], stddev=0.1), name="weights")
         b = tf.Variable(tf.zeros([outChannels]), name="biases")
@@ -16,7 +16,7 @@ def conv_layer(input, filterX, filterY, inChannels, outChannels, name='convoluti
         tf.summary.histogram("weights", w)
         tf.summary.histogram("biases", b)
         # model
-        res = tf.nn.relu(tf.nn.conv2d(input, w, strides=[1, 1, 1, 1], padding="SAME") + b)
+        res = tf.nn.relu(tf.nn.conv2d(input, w, strides=[1, xStride, yStride, 1], padding="SAME") + b)
         return res
 
 
@@ -47,11 +47,13 @@ def fc_softmax_layer(input, inSize, outSize, name='fc_softmax'):
 # Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
 mnist = mnist_data.read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
 
+print("train examples", mnist.train.num_examples)
+print("test examples", mnist.test.num_examples)
+
 IS = 28
-L0 = IS * IS * 4
-L1 = 200
-L2 = 60
-L3 = 30
+L1 = 14 * 14 * 4
+L2 = 7 * 7 * 16
+L3 = 200
 LN = 10
 
 # initialization
@@ -59,10 +61,10 @@ x = tf.placeholder(tf.float32, [None, IS, IS, 1], name="inputs")
 # placeholder for correct answers
 y = tf.placeholder(tf.float32, [None, LN], name="labels")
 
-res0 = conv_layer(x, 5, 5, 1, 4, "layer0")
-res1 = fc_relu_layer(tf.reshape(res0, [-1, L0]), L0, L1, 'layer1')
-res2 = fc_relu_layer(res1, L1, L2, 'layer2')
-res3 = fc_relu_layer(res2, L2, L3, 'layer3')
+res0 = conv_layer(x, 5, 5, 1, 4, name="layer0")
+res1 = conv_layer(res0, 4, 4, 4, 8, xStride=2, yStride=2, name="layer1")
+res2 = conv_layer(res1, 4, 4, 8, 16, xStride=2, yStride=2, name="layer2")
+res3 = fc_relu_layer(tf.reshape(res2, [-1, L2]), L2, L3, name='layer4')
 res = fc_softmax_layer(res3, L3, LN)
 
 init = tf.global_variables_initializer()
@@ -88,7 +90,9 @@ summary = tf.summary.merge_all()
 
 BASE_SUMMARY_DIR = "summary"
 VAR_SAVER_PATH = "model/saver/vars.ckpt"
-trainSteps = 10000
+batch = 100
+epoch = mnist.train.num_examples // batch
+trainSteps = epoch * 8
 showStep = 100
 lastStep = trainSteps - 1
 
@@ -102,7 +106,7 @@ with tf.Session() as sess:
     print("start:", time.ctime(start))
     for i in range(trainSteps):
         # load batch of images and correct answers
-        batchX, batchY = mnist.train.next_batch(100)
+        batchX, batchY = mnist.train.next_batch(batch)
         trainData = {x: batchX, y: batchY}
 
         # train and write statistics
