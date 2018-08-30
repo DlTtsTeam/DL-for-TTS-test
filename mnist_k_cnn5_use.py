ddeb0ff5844e30010ba0d75e120c60389306f6e6
ocmwdt <ocmwdt@gmail.com>
2018-08-30 11:02:58 +0400

    minor corrections

--- mnist_k_cnn5_use.py
+++ mnist_k_cnn5_use.py
@@ -6,17 +6,19 @@ from tensorflow.keras.models import *
 
 i_size = 28
 
+
 def load_and_process_image(path):
-    img = image.load_img(path, grayscale=True, target_size=(i_size, i_size), interpolation="bilinear")
+    img = image.load_img(path, grayscale=True, target_size=(i_size, i_size), interpolation="bicubic")
     img = ops.autocontrast(img)
     img = ops.invert(img)
-    img.save("test.jpg")
+    img.save("./pics/test.jpg")
     return img
+
 
 #load the image
 img = load_and_process_image('./pics/77.jpg')
-
 img = image.img_to_array(img)
+
 #useing the model
 model = load_model("./save/сnn5.h5")
 res = model.predict(img[None,:,:,:], batch_size=None, verbose=0, steps=None)
